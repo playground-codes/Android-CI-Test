5471d94750140abd22492b28ebdd8d3ff7a93810
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-05-17 11:55:19 +0900

    Add several tests

--- app/src/test/java/com/quangnguyen/androidcitest/ExampleUnitTest.java
+++ app/src/test/java/com/quangnguyen/androidcitest/ExampleUnitTest.java
@@ -1,5 +1,9 @@
 package com.quangnguyen.androidcitest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +14,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+  int[] samples = new int[]{2, 4, 12, 1, 5};
+
   @Test
   public void addition_isCorrect() throws Exception {
     assertEquals(4, 2 + 2);
@@ -17,6 +23,27 @@ public class ExampleUnitTest {
 
   @Test
   public void subtraction_isCorrect() {
-    assertEquals(1, 4 - 2);
+    assertEquals(1, 3 - 2);
+  }
+
+  @Test
+  public void sort_array() {
+    Arrays.sort(samples);
+    assertArrayEquals(samples, new int[]{1, 2, 4, 5, 12});
+  }
+
+  @Test
+  public void isEmpty() {
+    String hello = "Hello Test";
+    assertFalse(hello.isEmpty());
+    
+    String anotherHello = "";
+    assertTrue(anotherHello.isEmpty());
+
+    List<Integer> list = new ArrayList<>(100);
+    assertTrue(list.isEmpty());
+    list.add(1);
+    list.add(2);
+    assertFalse(list.isEmpty());
   }
 }
